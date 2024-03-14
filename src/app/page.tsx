8b34e68adc12cef78b7a2c429429dd789059fc80
Mohamed <mohammedmohamuud.01@gmail.com>
2024-03-15 00:43:07 +0300

    Completed the Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import MaxWidthWrapper from "@/Components/MaxWidthWrapper";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 import { buttonVariants } from "@/Components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -44,6 +45,106 @@ export default function Home() {
               }}
               className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-40  sm:left-[calc50%-33rem] sm:w-[72.1875rem]"
             />
+          </div>
+          <div>
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image
+                    src="/dashboard-preview.jpg"
+                    alt="product preview"
+                    width={1364}
+                    height={866}
+                    quality={100}
+                    className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-40  sm:left-[calc50%-36rem] sm:w-[72.1875rem]"
+            />
+          </div>
+        </div>
+      </div>
+      {/*Features section*/}
+      <div className="mx-auto mb-12 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-5xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              Starting chatting in minutes
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              Chatting with your PDF has never been easier than with Quill.
+            </p>
+          </div>
+        </div>
+        {/* Steps */}
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-1-4 border-zinc-300 py-2 pl-4 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Step 1</span>
+              <span className="text-xl font-semibold">
+                Sign up for an account
+              </span>
+              <span className="mt-2 text-zinc-700">
+                Either starting out with a free a plan or choose our{" "}
+                <Link
+                  href="/pricing"
+                  className="text-blue-700 underline underline-offset-2"
+                >
+                  pro plan
+                </Link>
+                .
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-1-4 border-zinc-300 py-2 pl-4 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Step 2</span>
+              <span className="text-xl font-semibold">
+                Upload your PDF file
+              </span>
+              <span className="mt-2 text-zinc-700">
+                We&apos;ll process your file and make it ready for you to chat
+                with.
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-1-4 border-zinc-300 py-2 pl-4 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Step 3</span>
+              <span className="text-xl font-semibold">
+                Start asking questions
+              </span>
+              <span className="mt-2 text-zinc-700">
+                It&apos;s that simple. Try out Quill today - it really takes
+                less than a minute.
+              </span>
+            </div>
+          </li>
+        </ol>
+        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <Image
+                src="/file-upload-preview.jpg"
+                alt="product preview"
+                width={1419}
+                height={732}
+                quality={100}
+                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+              />
+            </div>
           </div>
         </div>
       </div>
